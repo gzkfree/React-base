75de563eac5de943bd4bda2cb8c63acf399eb40a
gzkfree <gzkfree@163.com>
2020-10-30 01:38:16 +0800

    路由

--- src/common/router.js
+++ src/common/router.js
@@ -2,26 +2,21 @@ export default [
   {
     title: '微官网', // 页面标题&一级nav标题
     icon: 'icon-home',
-    name: '页面设计',  // 次级nav标题
-    meta: {
-      title: '页面设计'
-    },
     path: '/front/approval/undo', // 路由url
     component: 'minweb'  // 路由组件
   },
   {
     title: '会员', // 页面标题&一级nav标题
     icon: 'icon-home',
-    subMenu: 'vip',
+    path: '/vip',
     children: [{
-      navItem: true,
-      name: '会员列表',  // 次级nav标题
-      path: '/front/approval/undo', // 路由url
+      title: '会员列表',  // 次级nav标题
+      path: '/vip/List', // 路由url
       component: 'vipList'  // 路由组件
     },
     {
-      name: '会员卡列表',  // 次级nav标题
-      path: '/front/approval/undo', // 路由url
+      title: '会员卡列表',  // 次级nav标题
+      path: '/vip/cardList', // 路由url
       component: 'vipCardList'  // 路由组件
     }]
   },
--- src/components/asideMenu.js
+++ src/components/asideMenu.js
@@ -1,47 +1,53 @@
 import React, { Component } from "react";
 import { Menu } from 'antd';
 const { SubMenu } = Menu;
-import routeData from '../common/router'
+import Router from '../common/router'
 import {
-    DesktopOutlined,
-    PieChartOutlined,
-    FileOutlined,
-    TeamOutlined,
-    UserOutlined,
-  } from '@ant-design/icons';
-export default class AsideMenu extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return(
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            {
-              routeData.map((v, index) => {
-                return v.children ?
-                  <SubMenu
-                    key={v.subMenu}
-                    title={
-                      <span>
-                        <UserOutlined />
-                        <span>{v.title}</span>
-                      </span>
-                    }
-                  >{
-                      v.children.map(((v2, index2) => {
-                        return <Menu.Item key={v2.component}>{v2.name}</Menu.Item>
-                      }))
-                    }
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+export default class AsideMenu extends Component {
+  constructor(props) {
+    super(props)
+  }
+  // 无级菜单
+  // <Menu.Item key={v.component}>
+  //     控制台
+  // </Menu.Item>
+  // 子级菜单
+  // <SubMenu>
+  //   <Menu.Item key={v2.component}>{v2.name}</Menu.Item>
+  // </SubMenu>
 
-                  </SubMenu> :
-                  <Menu.Item key={v.component}>
-                    <PieChartOutlined />
-                    <span>{v.title}</span>
-                  </Menu.Item>
-              })
-            }
+  // 无级菜单处理
+  renderMenu({ path, title }) {
+    return <Menu.Item key={path}>{title}</Menu.Item>
+  }
+  // 子级菜单处理
+  renderSubMenu({ title, path, children }) {
+    return (
+      <SubMenu key={path} icon={<UserOutlined />} title={title}>
+        {
+          children && children.map(item => {
+            return item.children && item.children.length > 0 ? this.renderSubMenu(item) : this.renderMenu(item)
+          })
+        }
+      </SubMenu>
+    )
+  }
+  render() {
+    return (
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        {
+          Router && Router.map(firstItem => {
+            return firstItem.children && firstItem.children.length > 0 ? this.renderSubMenu(firstItem) : this.renderMenu(firstItem)
+          })
+        }
 
-          </Menu>
-        )
-    }
+      </Menu>
+    )
+  }
 }
